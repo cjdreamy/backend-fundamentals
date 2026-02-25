75f6378b798932e942f53d0e64543c45e2afc28e
cjdreamy <josephalocye402@gmail.com>
2026-02-25 16:23:09 +0300

    set up server

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+const PORT = 3000;
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.static("views"));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) =>{
+    res.render('index');
+})
+
+app.listen(PORT, (error) =>{
+    if(error){
+        console.log(err.message);
+    }
+    console.log(`server is running on port ${PORT}`)
+}
+)
