823f8425f619262c1d74304bad4fe05e226532c1
cjdreamy <josephalocye402@gmail.com>
2026-02-25 16:04:52 +0300

    sending to backend

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const submit_Button = document.getElementById('submit_Button');
 let Errors = []
 
-submit_Button.addEventListener('click', (e) => {
+submit_Button.addEventListener('click', async (e) => {
     e.preventDefault();
 
 let user_username = document.getElementById('user_Name').value.trim();
@@ -16,5 +16,17 @@ if(!email){
     Errors.push("Enter a valid email")
     return;
 }
+//send to backend
+
+const response = await fetch('/register', {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({user_Name, email})
+});
+
+const result = await response.text();
+alert(result);
 
 })
