53c4c8ab8b0dd447962fc523f951147d005dbeef
cjdreamy <josephalocye402@gmail.com>
2026-02-25 15:53:01 +0300

    data validation and sanitization

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-const user_username = document.getElementById('user_Name');
-const email = document.getElementById('email');
+
 const submit_Button = document.getElementById('submit_Button');
+let Errors = []
+
+submit_Button.addEventListener('click', (e) => {
+    e.preventDefault();
+
+let user_username = document.getElementById('user_Name').value.trim();
+let email = document.getElementById('email').value.trim();
+
+if(!username){
+    Errors.push("Name cannot be blank")
+    return;
+}
+if(!email){
+    Errors.push("Enter a valid email")
+    return;
+}
+
+})
